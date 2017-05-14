e67a5f1565cd6eee9573968d92bb3b878710416a
Kimran D <kimran_06@live.com>
2017-05-14 03:08:46 +0300

    ThreadsExtension

---
+++ GZipTest/Threading/ThreadsExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GZipTest.Threading
+{
+    static class ThreadsExtension
+    {
+        public static void StartThreadsWithIndex(this IEnumerable<Thread> threads)
+        {
+            int index = 0;
+            foreach (var thread in threads)
+            {
+                thread.Start(index);
+                index++;
+            }
+        }
+
+        public static void StartThreads(this IEnumerable<Thread> threads)
+        {
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+        }
+
+        public static WaitThreadResult WaitThreads(this IEnumerable<Thread> threads)
+        {
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            return new WaitThreadResult();
+        }
+
+        public static void ContinueWith(this WaitThreadResult waitResult, Action actionToRun)
+        {
+            actionToRun.Invoke();
+        }
+
+    }
+}
